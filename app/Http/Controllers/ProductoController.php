4b593c51265f9abbb8a343da3c51c302c925e907
Domingo Riviere <riviered@DC1.MDS>
2026-03-17 13:20:17 -0300

    Reemplazando las 7 rutas por una sola y pasando parametros a las vistas

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -10,4 +10,31 @@ class ProductoController extends Controller
     public function index() {
         return view("listado_productos");
     }
+
+    public function create() {
+        return "Creacion de un nuevo producto";
+    }
+
+    public function store(Request $request) {
+        return "Producto guardado correctamente";
+    }
+
+    public function show($id) {
+        // return view("show", ["id" => $id]); // forma 1 de pasar parametros a la vista (si aquí se pone "codigo" en vez de "id", en la vista se debe usar $codigo para acceder al valor)
+        // return view("show")->with("id", $id); // forma 2 de pasar parametros a la vista
+        $nombre = "Nombre del Producto $id";
+        return view("show", compact("id", "nombre")); // forma 3 de pasar parametros a la vista
+    }
+
+    public function edit($id) {
+        return "Editando producto";
+    }
+
+    public function update(Request $request, $id) {
+        return "Producto actualizado correctamente";
+    }
+
+    public function destroy($id) {
+        return "Producto eliminado correctamente";
+    }
 }
---
+++ resources/views/show.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vamo Carajo!</title>
+</head>
+<body>
+    <h1>Detalles del Producto {{ $id }}</h1>
+    <h3>{{ $nombre }}</h3>
+</body>
+</html>
