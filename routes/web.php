b005d664718031f63fc1711da4fbd074d9884d8e
Domingo <adomingor@hotmail.com>
2026-03-18 19:49:25 -0300

    Se instaló Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductoController;
-
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('welcome');
 });
 
-//                                         // 7 rutas necesarias de un circuito CRUD
-// /* 1 - Listado de registros */
-// Route::get('productos', [ProductoController::class, "index"])->name('productos.index');
-// /* 2 - Formulario de alta */
-// Route::get('productos/crear', function() {
-//     return "Form alta de productos";
-// });
-// /* 3 - Procesar el formulario de alta */
-// Route::post('productos', function() {
-//     return "Proceso de formulario de alta de productos";
-// });
-// /* 4 - Mostrar un registro */
-// Route::get('productos/{id}', function ($id) {
-//     return "Muestra los datos del producto $id";
-// });
-// /* 5 - Formulario de edición */
-// Route::get('productos/{id}/editar', function ($id) {
-//     return "Mustra el formulario de edición de un producto ($id)";
-// });
-// /* 6 - Procesar el formulario de edición */
-// Route::put('productos/{id}', function ($id) {
-//     return "Proceso del formulario de actualización de productos";
-// });
-// /* 7 - Eliminar un registro */
-// Route::delete('productos/{id}', function ($id) {
-//     return "Se procesa el borrado del producto";
-// });
-//                                         // FIN 7 rutas necesarias de un circuito CRUD
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Si solo necesitamos las 7 rutas necesarias de un circuito CRUD, podemos usar el método resource() de Route
-Route::resource('productos', ProductoController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
